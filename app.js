473062c88fda49707c1dd3bc090c5f24f3a29ea0
ThunyatepZ <taptitan4@gmail.com>
2022-01-19 11:16:52 +0700

    add product

--- app.js
+++ app.js
@@ -3,22 +3,33 @@ const chalk = require('chalk')
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const productRouter = express.Router();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(morgan('combined'));
-app.use(express.static(path.join(__dirname,"/public/")));
+app.use(express.static(path.join(__dirname, "/public/")));
 
-app.set("views","./src/views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs")
 
-app.get("/", (req,res) =>{
+productRouter.route("/").get((req, res) => {
+    res.send("Hello world");
+});
 
-    res.render('index',{username : 'Thunyatep++', customer: ['mos', 'moos', 'mss']})
+productRouter.route("/1").get((req, res) => {
+    res.send("Hello boi");
+});
+
+app.use("/products", productRouter)
+
+app.get("/", (req, res) => {
+
+    res.render('index', { username: 'Thunyatep++', customer: ['mos', 'moos', 'mss'] })
 
 })
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     debug(PORT)
 })
